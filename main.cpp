f055ef72dcdadaeb17c00b2045b5515be0b515ac
Тим Муранов <139687295+TimmMuranov@users.noreply.github.com>
2025-01-14 21:27:47 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,23 +9,18 @@
 using namespace std;
 
 int main() {
-    string list_of_words = "слово слово1 слово2";
-    string text = "Это тестовый текст, где встречаются такие слова как слово, слово1 и слово2.";
-
-    // Пример вызова без сортировки (по убыванию)
+    string list_of_words;
+    cout << "Введите список слов..." << endl;
+    cin >> list_of_words;
+    
+    string text;
+    cout << "Введите текст..." << endl;
+    cin >> text;
+    
     vector<string> result = countWordsInText(list_of_words, text);
-    cout << "Без сортировки:" << endl;
     for (const auto& res : result) {
         cout << res << endl;
     }
-
-    // Пример вызова с сортировкой по возрастанию
-    vector<string> result_reverse = countWordsInText(list_of_words, text, true);
-    cout << "\nСортировка по возрастанию:" << endl;
-    for (const auto& res : result_reverse) {
-        cout << res << endl;
-    }
-
     return 0;
 }
 
